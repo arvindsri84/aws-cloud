6947ae34be9ba0f836643bdbf08948d342c27788
arvindsri84 <arvinddotsrivastava@gmail.com>
2023-08-14 22:18:36 +0530

    Infrastructure setup for 1. AWS Lambda with docker container and 2. Autoscaling EC2 with loadbalancers

---
+++ aws-lambda-container-handler/src/main/java/in/arvindsri82/cloud/App.java
@@ -0,0 +1,7 @@
+package in.arvindsri82.cloud;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ aws-lambda-container-handler/src/test/java/in/arvindsri82/cloud/AppTest.java
@@ -0,0 +1,9 @@
+package in.arvindsri82.cloud;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    public void testApp() {
+        assertTrue(true);
+    }
+}
